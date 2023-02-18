111c4e1ac8b5f67fbf6b295a0c2a1805f8ddc99e
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-19 00:52:50 +0600

    Header responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 import logo_white from "../../public/logo_white.png";
+import logo from "../../public//logo.svg";
 import { AiOutlineUser, AiOutlinePlus } from "react-icons/Ai";
+import { FaBars, FaUser } from "react-icons/fa";
 
 function Navbar() {
   const menuItems = (
@@ -29,15 +31,20 @@ function Navbar() {
       {/* Navbar */}
       <div className="bg-blue-200 px-4 md:px-8 border-b-2 mb-4 py-3.5 pt-5 sticky top-0 z-50">
         <div className="flex justify-between items-center relative container">
+          {/* Mobile menu button */}
+          <button className="text-white bg-main p-2.5 text-2xl rounded xl:hidden hover:bg-red-600 transition-all ">
+            <FaBars />
+          </button>
+
           <Link href={"/"} className="flex justify-center items-center">
-            <Image width={150} height={47} src={logo_white} alt="" />
+            <Image width={150} height={47} src={logo} alt="" />
           </Link>
 
-          <div className=" flex items-center gap-6">
-            <nav className="text-base font-semibold text-[#303030] space-x-[3.7rem] hidden lg:block ">
+          <div className=" xl:flex hidden items-center gap-6">
+            <nav className="text-base font-semibold text-[#303030] space-x-[3.7rem]  ">
               {menuItems}
             </nav>
-            <div className="text-md xl:text-xl  hidden lg:flex gap-7 justify-between items-center">
+            <div className="text-md flex gap-7 justify-between items-center">
               <div className="flex gap-1 items-center justify-center border-l-[1px] pl-5">
                 <AiOutlineUser className="text-2xl" />
                 <Link href={"#"} className="text-base">
@@ -53,12 +60,11 @@ function Navbar() {
 
           {/* custom hamburger button */}
 
-          {/* mobile menu */}
-          <div
-            className={`  absolute  flex gap-3 flex-col text-md text-[#303030]  lg:hidden bg-white w-full py-5 px-5 border-b-4 transition-all duration-500`}
-          >
-            {menuItems}
-          </div>
+          {/* mobile menu user button */}
+
+          <button className="text-2xl xl:hidden">
+            <FaUser />
+          </button>
         </div>
       </div>
     </>
