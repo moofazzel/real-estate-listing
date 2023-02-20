af7327c552ec221cbcd432d7af2dc01bd90d5f98
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-20 10:46:44 +0600

    banner text center

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,3 +1,14 @@
+import banner1 from "../../../public/slider1.jpg";
+
+function Banner() {
+  return (
+    <>
+      <div className=" heroBanner h-52 w-screen">This is banner</div>
+    </>
+  );
+}
+
+export default Banner;
 function Banner() {
   return (
     <>
