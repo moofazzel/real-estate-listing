68eae0f2d14b708eb175a5cf7f73e1702e87a2c7
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-20 09:39:00 +0600

    Header responsive logo and color also icon added

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,15 @@
+import banner1 from "../../../public/slider1.jpg";
+
+function Banner() {
+  return (
+    <>
+      <div
+        className=" heroBanner h-52 w-screen"
+      >
+        This is banner
+      </div>
+    </>
+  );
+}
+
+export default Banner;
