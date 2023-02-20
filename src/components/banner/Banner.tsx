7b12ba921324ddb9af783aeeb14994598d52512d
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-20 10:47:39 +0600

    banner make text center

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,14 +1,3 @@
-import banner1 from "../../../public/slider1.jpg";
-
-function Banner() {
-  return (
-    <>
-      <div className=" heroBanner h-52 w-screen">This is banner</div>
-    </>
-  );
-}
-
-export default Banner;
 function Banner() {
   return (
     <>
