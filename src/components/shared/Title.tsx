7dbb1daa24b8b4a527b3f6da38f9fee9f3c82265
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-21 09:40:55 +0600

    properties by cities done

--- src/components/shared/Title.tsx
+++ src/components/shared/Title.tsx
@@ -1,7 +1,7 @@
 function Title(props: { title: string }) {
   return (
     <>
-      <h2 className="text-titleColor text-3xl font-bold text-center">
+      <h2 className="text-titleColor text-3xl font-bold text-center mb-3">
         {props.title}
       </h2>
     </>
