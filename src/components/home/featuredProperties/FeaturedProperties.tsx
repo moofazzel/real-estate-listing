bd595fd03aa981ca6bf76f2a8a821a3905b320f8
jakia12 <owasi110@gmail.com>
2023-02-21 19:56:33 +0600

    Property card is done

--- src/components/home/featuredProperties/FeaturedProperties.tsx
+++ src/components/home/featuredProperties/FeaturedProperties.tsx
@@ -1,7 +1,9 @@
 import { TfiLocationPin } from "react-icons/tfi";
 import Image from "next/image";
-
-import sliderImg from "../../../../public/sl2.jpg";
+import sliderImg from "../../../../public/assets/properties/sl2.jpg";
+import agentImage from "../../../../public/assets/agents/ag1.jpg";
+import { RiHeart2Line } from "react-icons/ri";
+import { BiTransfer } from "react-icons/bi";
 
 const FeaturedProperties = () => {
   return (
@@ -9,16 +11,48 @@ const FeaturedProperties = () => {
       <div className="container mx-auto px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="border border-gray-300 rounded">
-            <Image
-              src={sliderImg}
-              alt="it's a slider image"
-              width={280}
-              height={280}
-            />
+            <div className="m-3 relative rounded-md z-10">
+              <Image
+                src={sliderImg}
+                alt="it's a slider image"
+                className="w-full  rounded-md"
+              />
+              <div className="before:content-['*'] before:w-full before:h-full before:absolute before:top-0 before:left-0 before:bg-slate-600 before:opacity-[0.5] before:rounded-md "></div>
+              <div className="absolute top-3 left-3">
+                <div className="flex items-center">
+                  <button className="py-1.5 px-3 bg-[#3e4c66] text-sm rounded-md text-white mr-3">
+                    For Rent
+                  </button>
+                  <button className="py-1.5 px-3 bg-main text-sm rounded-md text-white">
+                    Featured
+                  </button>
+                </div>
+              </div>
+
+              <div className="absolute w-full h-full px-[18px] top-[78%]">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-xl md:text-[22px] text-white font-semibold capitalize">
+                    $ 2202
+                  </h3>
+                  <div className="flex items-center">
+                    <button className="bg-[#11111193] p-2 text-white text-xl rounded-md mr-2">
+                      <RiHeart2Line />
+                    </button>
+                    <button className="bg-[#11111193] p-2 text-white text-xl rounded-md">
+                      <BiTransfer />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
             {/* card body */}
-            <div className=" p-4 ">
+            <div className=" px-4 py-2">
+              {/* property type */}
+              <span className="text-sm font-normal text-main mb-3">
+                Apartment
+              </span>
               {/* property title */}
-              <h3 className="text-lg font-medium text-dark my-3">
+              <h3 className="text-lg font-medium text-dark mb-3">
                 Diamond Mentor jlsdjkhgd
               </h3>
               {/* address */}
@@ -50,7 +84,13 @@ const FeaturedProperties = () => {
             <div className="border-t border-gray-300 px-4 py-2">
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                  <span className="w-[60px] h-[60px] rounded-full bg-teal-500 mr-3"></span>
+                  {/* agent image */}
+                  <Image
+                    src={agentImage}
+                    alt="Agent photo"
+                    className="w-[45px] h-[45px] rounded-full mr-3"
+                  />
+
                   <span className="text-sm font-normal text-gray-800 mr-3">
                     John Doe
                   </span>
