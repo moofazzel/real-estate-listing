ff3844fc5d9371fc95db43b5448ab746cc011639
jakia12 <owasi110@gmail.com>
2023-02-22 23:46:26 +0600

    articles and tips added

--- src/components/home/articlesAndTips/ArticlesTips.tsx
+++ src/components/home/articlesAndTips/ArticlesTips.tsx
@@ -1,5 +1,15 @@
+import SingleArticles from "@/components/singleArticles/SingleArticles";
+
 const ArticlesTips = () => {
-  return <div></div>;
+  return (
+    <section className="py-14 md:py-20">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <SingleArticles />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ArticlesTips;
