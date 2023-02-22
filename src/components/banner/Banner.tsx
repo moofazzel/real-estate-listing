ae672fba5eaf6da9b67c310715b52c5d1bacad3f
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2023-02-23 01:17:39 +0600

    Filter option added

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,15 +1,48 @@
+import { useForm } from "react-hook-form";
+
 function Banner() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: {}) => console.log(data);
+
   return (
     <>
-      <div className=" heroBanner h-screen w-full flex justify-center items-center pt-28">
-        <div className="text-center">
+      <div className=" heroBanner h-[42rem] w-full ">
+        <div className="text-center translate-y-96">
           <h1 className="text-white text-3xl xl:text-6xl font-bold">
             Find Your Dream Home
           </h1>
 
-          <p className="text-white text-lg font-bold pb-2">
+          <p className="text-white text-lg font-bold pb-2 rounded-lg">
             From as low as $10 per day with limited time offer discounts
           </p>
+
+          {/* Filter part */}
+
+          <div className="containers px-10">
+            <div className="bg-[#ffffff33] p-3 rounded-lg">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="bg-white p-5 rounded-lg "
+              >
+                <div>
+                  <input
+                    type="text"
+                    id="first_name"
+                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-main focus:border-main block w-full p-3 px-5"
+                    placeholder="Enter Search Keyword..."
+                    {...register("keyword")}
+                  />
+                </div>
+
+                <input type="Search" />
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
